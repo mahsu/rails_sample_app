b6fbeed0aa35c5bdc95e206be1ea91735cfeab88
Matthew Hsu <gears88@gmail.com>
2014-12-25 00:37:56 -0500

    static pages controller

--- bootstrap.sh
+++
@@ -1,9 +0,0 @@
-apt-get update
-apt-get --assume-yes install curl
-gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-\curl -L http://get.rvm.io | bash -s stable
-sudo chmod -R a+xwr /usr/local/rvm
-source /etc/profile.d/rvm.sh
-rvm requirements
-rvm install ruby
-gem install rails --no-ri --no-rdoc
